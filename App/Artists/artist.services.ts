98f275db247a3b89122ce0116b0d1d2f642b061d
mb-tousif <meta.block.tousif@gmail.com>
2023-06-03 19:47:07 +0600

    eslint and prettire istalled

--- App/Artists/artist.services.ts
+++ App/Artists/artist.services.ts
@@ -1,8 +1,9 @@
 import { User } from "../Users/user.model";
 import { Artist } from "../Artists/artist.model";
+import { TUser } from "../Users/user.interfaces";
 // Update user to artist
 export const updateUserToArtist = async (payload: string) => {
-  const result:any = await User.findOne( { userId: payload } );
+  const result = await User.findOne( { userId: payload } ) as TUser;
   const newCollection = new Artist({
     userId: result.userId,
     email: result.email,
