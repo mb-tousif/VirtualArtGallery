59b919fc76d23fd53b9b41a62f993d2c02ad7c0d
mb-tousif <meta.block.tousif@gmail.com>
2023-06-13 13:49:44 +0600

    arts updated by id coded

--- App/Arts/art.routes.ts
+++ App/Arts/art.routes.ts
@@ -1,11 +1,11 @@
 import express from "express";
-import { createArt } from "./art.controller";
+import { createArt, updateArtById } from "./art.controller";
 import zodValidateRequest from "../middleware/zodValidateReq";
 import createArtZodSchema from "./art.zod.validation";
 const router = express.Router();
 
 router.post("/createArt", zodValidateRequest(createArtZodSchema), createArt);
-router.put("/updateArt/:id", );
+router.patch("/updateArt/:id", updateArtById);
 router.delete("/deleteArt/:id", );
 router.get("/getArtById/:id", );
 router.get("/getAllArts", );
